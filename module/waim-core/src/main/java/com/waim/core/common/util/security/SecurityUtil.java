23a8057b7c428dfaa36c14dc10a1f5a91261989e
shlee <noia1223@gmail.com>
2026-02-27 06:58:19 +0900

    .

--- module/waim-core/src/main/java/com/waim/core/common/util/security/SecurityUtil.java
+++ module/waim-core/src/main/java/com/waim/core/common/util/security/SecurityUtil.java
@@ -3,7 +3,6 @@ package com.waim.core.common.util.security;
 import com.waim.core.common.util.jwt.model.JwtUserDetail;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.core.userdetails.UserDetails;
 
 public class SecurityUtil {
     public static String getCurrentUserUid() {
